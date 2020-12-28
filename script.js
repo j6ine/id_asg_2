d150f6ab81b40cce36fc19cca2b5eb7af2277cea
j6ine <65351853+j6ine@users.noreply.github.com>
2020-12-28 22:41:20 +0800

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$.getJSON{
+    ""
+}
+
+function handleResponse(response) {
+    for (var i = 0; i < response.items.length; i++) {
+      var item = response.items[i];
+      // in production code, item.text should have the HTML entities escaped.
+      document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title;
+    }
+}
