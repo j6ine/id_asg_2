f511a884e5869df3b560d70a82b6780750ae1b4b
j6ine <65351853+j6ine@users.noreply.github.com>
2021-01-03 11:11:39 +0800

    new commit

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-$.getJSON{
-    ""
-}
+$(document).ready(function(){	
 
-function handleResponse(response) {
-    for (var i = 0; i < response.items.length; i++) {
-      var item = response.items[i];
-      // in production code, item.text should have the HTML entities escaped.
-      document.getElementById("content").innerHTML += "<br>" + item.volumeInfo.title;
+  $("#searchForm").submit(function(){
+
+    var search = $(".searchInput").val();
+    if(search == "")
+    {
+      alert("Please enter something in the field");
     }
-}
+
+    else
+    {		
+      var url = "";
+      var img = "";
+      var title = "";
+      var author = "";
+      var bttn = "";
+
+      $.get("https://www.googleapis.com/books/v1/volumes?q=" + search,function(response){
+        for(i=0;i<response.items.length;i++)
+        {
+          title=$('<h5 class="card-title">' + response.items[i].volumeInfo.title + '</h5>');  
+          author=$('<p class="card-text"> By:' + response.items[i].volumeInfo.authors + '</p>');
+          img = $('<a href="#" class="btn btn-primary">Read More' + response.items[i].volumeInfo.infoLink + '</a>'); 	
+          url= response.items[i].volumeInfo.imageLinks.thumbnail;
+          img.attr('src', url);
+
+          title.appendTo('#cardResult.card-body');
+          author.appendTo('#cardResult.card-body');
+          img.appendTo('#cardResult.card-body');
+
+          <img class="card-img-top" src="" alt="Card image cap"></img>
+        }
+
+      });
+      
+    }
+    
+    return false;
+  });
+
+});
