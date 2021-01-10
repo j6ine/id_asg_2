e406bfcb0cd039191aa9bfdeecf62edf5ed36f8f
j6ine <65351853+j6ine@users.noreply.github.com>
2021-01-10 20:03:24 +0800

    ajax updated

--- script.js
+++ script.js
@@ -1,52 +1,62 @@
-/* performing a search - future implement: concat eg. q=blah+intitle:blah+subject:
-var seacrh = 
-var searchBy = ["byIsbn", "byAuthor", "byName", "byPart", "by"]
-var searchby = ["intitle:", "inauthor:", "inpublisher:", "subject:", "isbn:", "lccn:", "oclc:"]
+const search = "";
+const terms = ""; 
+const limit = "";
 
-if 
-
-else if
-
-
-"https://www.googleapis.com/books/v1/volumes?q=" + searchBy + search
-
-
-
-*/
+/* function to return terms (parameter of api) */
+$(".search-dropdown").change(function(e){
+    var chosen = $(this).children(":selected").href();
+    if (chosen=="#isbn"){
+        terms = "isbn";
+    }
+    else if (chosen=="#part"){
+        terms = "intitle";
+    }
+    else if (chosen=="#subject"){
+        terms = "subject";
+    }
+    else if (chosen=="#author"){
+        terms = "inauthor";
+    }
+    else if (chosen=="#publisher"){
+        terms = "inpublisher";
+    }
+    else if (chosen=="#com-lib"){
+        terms = "oclc";
+    }
+    else if (chosen=="#congress-lib"){
+        terms = "lccn";
+    }
+    else{
+        terms = "intitle";
+    }
+})
 
 
 /* */
-
-/* */
-
-/* */
-
-/* */
-
-/* */
-
-/* */
-
 function bookSearch(){
     var search = $('.search-text').value;
     $('.search-result').innerHTML = ""
 
-    console.log(search)
-
     $.ajax({
-        url: "https://www.googleapis.com/books/v1/volumes?=" + search,
+        url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
         dataType: "json",
 
         success: function(data){
-            console.log(data)
+            for(i=0;i<response.items.length;i++){
+                title=$('<h5 class="center-align white-text">' + response.items[i].volumeInfo.title + '</h5>');  
+                author=$('<h5 class="center-align white-text"> By:' + response.items[i].volumeInfo.authors + '</h5>');
+                img = $('<img class="aligning card z-depth-5" id="dynamic"><br><a href=' + response.items[i].volumeInfo.infoLink + '><button id="imagebutton" class="btn red aligning">Read More</button></a>'); 	
+                url= response.items[i].volumeInfo.imageLinks.thumbnail;
+                img.attr('src', url);
+                title.appendTo('#search-result');
+                author.appendTo('#search-result');
+                img.appendTo('#search-result');
+            }
         },
 
         type: 'GET'
     });
 }
-
-
-var searchBtn = document.getElementsByClassName('search-btn');
 
 for (var i = 0; i < searchBtn.length; i++) {
     searchBtn[i].addEventListener('click', bookSearch); 
